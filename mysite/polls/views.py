a2dfe8ce4aa9b796577d227342f8ebcfab11001a
Иванов Евгений <ivanov4.1997@gmail.com>
2022-02-22 23:37:49 +0400

    fix race condition problem

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.db.models import F
 
 from .models import Question
 # Create your views here.
@@ -26,14 +27,13 @@ def results(request, question_id):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        _pk = request.POST['choice']
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html',
                         {'question': question, 'error_message': "You didn't select a choice.",})
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
+        question.choice_set.filter(pk=_pk).update(votes=F('votes') + 1)
     # Always return an HttpResponseRedirect after successfully dealing
     # with POST data. This prevents data from being posted twice if a
     # user hits the Back button.
